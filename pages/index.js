b0ccd81bd39e345e106a6c37ea329d6a142568c3
Jaco-Miras <miras.jaco@gmail.com>
2022-11-18 00:44:38 +0800

    Header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold">
-    Hello world!
-  </h1>
+   <div>
+    {/* Header */}
+    <Header />
+    {/* Feed */}
+
+    {/* Modal */}
+   </div>
   )
 }
